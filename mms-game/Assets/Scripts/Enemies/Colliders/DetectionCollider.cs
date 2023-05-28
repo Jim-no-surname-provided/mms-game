9116c4e03455b7e30a5c54e653b62d21f3b27119
Helmut Joaquín Pfeffer García <69730072+Jim-no-surname-provided@users.noreply.github.com>
2023-05-28 12:28:20 +0200

    DetectionCollider added

---
+++ mms-game/Assets/Scripts/Enemies/Colliders/DetectionCollider.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionCollider : MonoBehaviour
+{
+    public enum TypeOfDetection
+    {
+        WALL_DETECTION,
+        KILL_PLAYER,
+        DIE_FROM_PLAYER
+    }
+    public TypeOfDetection typeOfDetection;
+}
