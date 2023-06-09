458ac88bcd5149471b5afd06b834b678210f672f
arikyc <kycyku.arian@gmail.com>
2023-06-09 15:54:13 +0200

    Added Training

--- mms-game/Assets/Scripts/Enemies/ShieldedSimpleMonster/ShieldedSimpleMonster.cs
+++ mms-game/Assets/Scripts/Enemies/ShieldedSimpleMonster/ShieldedSimpleMonster.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class ShieldedSimpleMonster : SimpleMonster
 {
     [SerializeField] private DetectionCollider shieldDetector;
     [SerializeField] private DetectionCollider weakPointDetector;
+    [SerializeField] private float directionChangeInterval = 0f;
+    private Coroutine directionChangeCoroutine;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -29,6 +32,20 @@ public class ShieldedSimpleMonster : SimpleMonster
         });
 
         SetWallDetector();
+
+        if (directionChangeInterval > 0)
+        {
+            directionChangeCoroutine = StartCoroutine(ChangeDirectionCoroutine());
+        }
+    }
+
+    IEnumerator ChangeDirectionCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(directionChangeInterval);
+            ChangeMovingDirection();
+        }
     }
 
     public override void ChangeMovingDirection()
@@ -37,4 +54,11 @@ public class ShieldedSimpleMonster : SimpleMonster
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
+    private void OnDestroy()
+    {
+        if (directionChangeCoroutine != null)
+        {
+            StopCoroutine(directionChangeCoroutine);
+        }
+    }
 }
